cd4ab4b6aa523ca64d01c0a6aa98c11305fab19d
ArgentLo <argent.lo.hk@gmail.com>
2021-03-17 03:45:53 +0900

    add more base model

--- tawara_train.py
+++ tawara_train.py
@@ -1,3 +1,4 @@
+from config import DEBUG
 import gc
 import sys
 sys.path.append('../extension/pytorch-image-models-master')
@@ -20,11 +21,7 @@ from tawara_utils  import *
 # Numpy dataset
 ##################################################
 
-train_data_arr = np.load("./dataset/train_640x640.npy", mmap_mode="r")
-
-# for p in DATA.iterdir():
-#     print(p.name)
-
+train_data_arr = np.load("./dataset/train_512x512.npy", mmap_mode="r")
 train = pd.read_csv(DATA / "train.csv")
 smpl_sub =  pd.read_csv(DATA / "sample_submission.csv")
 
@@ -35,47 +32,29 @@ smpl_sub =  pd.read_csv(DATA / "sample_submission.csv")
 label_arr = train[CLASSES].values
 group_id = train.PatientID.values
 
-train_val_indexs = list(
-    multi_label_stratified_group_k_fold(label_arr, group_id, N_FOLD, RANDAM_SEED))
+train_val_indexs = list(multi_label_stratified_group_k_fold(label_arr, group_id, N_FOLD, RANDAM_SEED))
 
 train["fold"] = -1
 for fold_id, (trn_idx, val_idx) in enumerate(train_val_indexs):
     train.loc[val_idx, "fold"] = fold_id
-    
-train.groupby("fold")[CLASSES].sum()
-
-
-##################################################
-# forward test
-##################################################
-
-# m = SingleHeadModel("resnext50_32x4d", 11, True)
-m = MultiHeadResNet200D([3, 4, 3, 1], True)
-m = m.eval()
-
-x = torch.rand(1, 3, 256, 256)
-with torch.no_grad():
-    y = m(x)
-print("[forward test]")
-print("input:\t{}\noutput:\t{}".format(x.shape, y.shape))
-
-del m; del x; del y
-gc.collect()
 
 ##################################################
 # Train Function
 ##################################################
 
-def run_train_loop(manager, stgs, model, device, train_loader, optimizer, scheduler, loss_func):
+def run_train_loop(manager, stgs, model, device, train_loader, optimizer, scheduler, loss_func, VAL_FOLD_ID):
     """Run minibatch training loop"""
     step_scheduler_by_epoch, step_scheduler_by_iter = get_stepper(manager, stgs, scheduler)
 
     if stgs["globals"]["use_amp"]:
         print(">>> Using AMP Training...")
+
+        EPOCH = 1
         while not manager.stop_trigger:
             model.train()
             scaler = torch.cuda.amp.GradScaler()
-            for x, t in tqdm(train_loader):
+            
+            for x, t in train_loader:
                 with manager.run_iteration():
                     x, t = x.to(device), t.to(device)
                     optimizer.zero_grad()
@@ -87,23 +66,14 @@ def run_train_loop(manager, stgs, model, device, train_loader, optimizer, schedu
                     scaler.step(optimizer)
                     scaler.update()
                     step_scheduler_by_iter()
+
+            # save every epoch
+            save_name = f'{SAVED_MODEL_PATH}{BACKBONE}_Fold{VAL_FOLD_ID}_Size{IMG_SIZE}_Epoch{EPOCH}.pth'
+            torch.save(model.state_dict(), save_name)
+            EPOCH += 1
+
             step_scheduler_by_epoch()
-    else:
-        while not manager.stop_trigger:
-            model.train()
-            for x, t in train_loader:
-                with manager.run_iteration():
-                    x, t = x.to(device), t.to(device)
-                    optimizer.zero_grad()
-                    y = model(x)
-                    loss = loss_func(y, t)
-                    ppe.reporting.report({'train/loss': loss.item()})
-                    loss.backward()
-                    optimizer.step()
-                    step_scheduler_by_iter()
-            step_scheduler_by_epoch()
-        
-        
+
 def run_eval(stgs, model, device, batch, eval_manager):
     """Run evaliation for val or test. this function is applied to each batch."""
     model.eval()
@@ -121,23 +91,26 @@ def run_eval(stgs, model, device, batch, eval_manager):
 # Train One Fold
 ##################################################
 
-def train_one_fold(settings, train_all, output_path, print_progress=False):
+def train_one_fold(settings, train_all, output_path, VAL_FOLD_ID, print_progress=False):
     """train one fold"""
     torch.backends.cudnn.benchmark = True
     set_random_seed(settings["globals"]["seed"])
 
-    # # prepare train, valid paths
-    # train_file_list, val_file_list = get_file_list(settings, train_all, "png")
+    # prepare train, valid paths
     train_file_list, val_file_list = get_file_list_with_array(settings, train_all)
     print("train: {}, val: {}".format(len(train_file_list), len(val_file_list)))
-
     device = torch.device(settings["globals"]["device"])
-    # # get data_loader
+    
+    # get data_loader
     train_loader, val_loader = get_dataloaders_cls(
         settings, train_file_list, val_file_list, LabeledImageDatasetNumpy)
 
-    # # get model
-    model = MultiHeadResNet200D(**settings["model"]["params"])
+    # get model
+    # model = MultiHead_DenseNet121(**settings["model"]["params"])
+    # model = MultiHead_SeResNet152d(**settings["model"]["params"])
+    # model = MultiHeadResNet200D(**settings["model"]["params"])
+    # model = MultiHead_EffNetb5(**settings["model"]["params"])
+    model = MultiHead_InceptionV3(**settings["model"]["params"])
     model.to(device)
 
     # # get optimizer
@@ -153,11 +126,10 @@ def train_one_fold(settings, train_all, output_path, print_progress=False):
         torch.optim.lr_scheduler, settings["scheduler"]["name"]
     )(optimizer, **settings["scheduler"]["params"])
 
-    # # get loss
-    if hasattr(nn, settings["loss"]["name"]):
-        loss_func = getattr(nn, settings["loss"]["name"])(**settings["loss"]["params"])
-    else:
-        loss_func = eval(settings["loss"]["name"])(**settings["loss"]["params"])
+    # get loss
+    ClassWeights = torch.tensor([1 for _ in range(7)] + [2]*3 + [1])
+    ClassWeights = ClassWeights.to(device)
+    loss_func = nn.BCEWithLogitsLoss(pos_weight=ClassWeights)
     loss_func.to(device)
 
     eval_manager = EvalFuncManager(
@@ -167,8 +139,7 @@ def train_one_fold(settings, train_all, output_path, print_progress=False):
         })
     eval_manager.to(device)
 
-    # # get manager
-    # trigger = None
+    # get manager
     trigger = ppe.training.triggers.EarlyStoppingTrigger(
         check_trigger=(1, 'epoch'),
         # monitor='val/metric', mode="min",
@@ -184,10 +155,10 @@ def train_one_fold(settings, train_all, output_path, print_progress=False):
     manager = set_extensions(
         manager, settings, model, device, val_loader, optimizer, eval_manager, print_progress)
 
-    # # run training.
-    run_train_loop(
-        manager, settings, model, device, train_loader,
-        optimizer, scheduler, loss_func)
+    # run training.
+    run_train_loop(manager, settings, model, device, train_loader,
+                   optimizer, scheduler, loss_func, VAL_FOLD_ID)
+
 
 
 ##################################################
@@ -219,38 +190,9 @@ torch.cuda.empty_cache()
 gc.collect()
 
 for fold_id, tmp_stgs in zip(FOLDS, stgs_list):
-    train_one_fold(tmp_stgs, train, TMP / f"fold{fold_id}", False)
+
+    print(f">>> Current Fold_ID: {fold_id}")
+
+    train_one_fold(tmp_stgs, train, TMP / f"fold{fold_id}", VAL_FOLD_ID=fold_id, print_progress=True)
     torch.cuda.empty_cache()
     gc.collect()
-
-
-##################################################
-# Eval OOF and Get the BEST Model
-##################################################
-
-best_log_list = []
-for fold_id, tmp_stgs in zip(FOLDS, stgs_list):
-    exp_dir_path = TMP / f"fold{fold_id}"
-    log = pd.read_json(exp_dir_path / "log")
-    best_log = log.iloc[[log["val/metric"].idxmax()],]
-    best_epoch = best_log.epoch.values[0]
-    best_log_list.append(best_log)
-    
-    best_model_path = exp_dir_path / f"snapshot_epoch_{best_epoch}.pth"
-    copy_to = f"./best_model_fold{fold_id}.pth"
-    shutil.copy(best_model_path, copy_to)
-    
-    for p in exp_dir_path.glob("*.pth"):
-        p.unlink()
-    
-    shutil.copytree(exp_dir_path, f"./fold{fold_id}")
-    
-    with open(f"./fold{fold_id}/settings.yml", "w") as fw:
-        yaml.dump(tmp_stgs, fw)
-    
-pd.concat(best_log_list, axis=0, ignore_index=True)
-
-
-##################################################
-# 
-##################################################
