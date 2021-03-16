1d263eaccb0cf11063c0f236f3cc2aa17359a7d1
ArgentLo <argent.lo.hk@gmail.com>
2021-03-17 03:45:36 +0900

    adjust learning rate

--- tawara_config.py
+++ tawara_config.py
@@ -1,7 +1,9 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 from pathlib import Path
-
+IMG_SIZE = 512
+BACKBONE =  "MultiHead_InceptionV3"  # "MultiHead_DenseNet121" "MultiHead_SeResNet152d" "MultiHeadResNet200D" "MultiHead_EffNetb5"
+DEBUG = False
 
 ROOT   = Path.cwd()
 # INPUT  = ROOT / "input"
@@ -19,7 +21,7 @@ N_CLASSES = 11
 # N_FOLD = len(FOLDS)
 FOLDS = [0, 2, 4]
 N_FOLD = 5
-
+SAVED_MODEL_PATH = "./saved_models/"
 CLASSES = [
     'ETT - Abnormal', 'ETT - Borderline', 'ETT - Normal',
     'NGT - Abnormal', 'NGT - Borderline', 'NGT - Incompletely Imaged', 'NGT - Normal',
@@ -28,11 +30,11 @@ CLASSES = [
 
 stgs_str = """
 globals:
-  seed: 1086
+  seed: 4213
   device: cuda
-  max_epoch: 16
+  max_epoch: 15
   patience: 3
-  dataset_name: train_640x640
+  dataset_name: train_512x512
   use_amp: True
   val_fold: 0
   debug: False
@@ -43,9 +45,9 @@ dataset:
     transform_list:
       - [HorizontalFlip, {p: 0.5}]
       - [ShiftScaleRotate, {
-          p: 0.5, shift_limit: 0.2, scale_limit: 0.2,
+          p: 0.65, shift_limit: 0.2, scale_limit: 0.2,
           rotate_limit: 20, border_mode: 0, value: 0, mask_value: 0}]
-      - [RandomResizedCrop, {height: 512, width: 512, scale: [0.9, 1.0]}]
+      - [RandomResizedCrop, {height: 512, width: 512, scale: [0.8, 1.2]}]
       - [Cutout, {max_h_size: 51, max_w_size: 51, num_holes: 5, p: 0.5}]
       - [Normalize, {
           always_apply: True, max_pixel_value: 255.0,
@@ -59,13 +61,12 @@ dataset:
       - [ToTensorV2, {always_apply: True}]
 
 loader:
-  train: {batch_size: 8, shuffle: True, num_workers: 2, pin_memory: True, drop_last: True}
-  val: {batch_size: 32, shuffle: False, num_workers: 2, pin_memory: True, drop_last: False}
+  train: {batch_size: 64, shuffle: True, num_workers: 8, pin_memory: True, drop_last: True}
+  val: {batch_size: 16, shuffle: False, num_workers: 2, pin_memory: True, drop_last: False}
 
 model:
-  name: MultiHeadResNet200D
+  name: MultiHead_InceptionV3 # MultiHead_DenseNet121 MultiHeadResNet200D MultiHead_SeResNet152d MultiHead_EffNetb5
   params:
-    # base_name: resnet200D_320
     out_dims_head: [3, 4, 3, 1]
     pretrained: True
 
@@ -78,7 +79,7 @@ eval:
 optimizer:
     name: Adam
     params:
-      lr: 2.5e-04
+      lr: 6.5e-05  # ResNet200d: 2.5e-04 || SeResNet: 1.e-04
 
 scheduler:
   name: CosineAnnealingWarmRestarts
@@ -86,9 +87,3 @@ scheduler:
     T_0: 16
     T_mult: 1
 """
-
-
-
-
-
-
